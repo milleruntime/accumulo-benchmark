74fb7fd835825a11f43b978761afad779bd85fd0
Mike Miller <mmiller@apache.org>
2017-05-17 14:17:09 -0400

    Added Version printing

--- jmh-test/src/main/java/org/apache/accumulo/TestUtils.java
+++ jmh-test/src/main/java/org/apache/accumulo/TestUtils.java
@@ -5,17 +5,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.TestIteratorStack;
+import org.apache.commons.cli.Options;
 import org.openjdk.jmh.annotations.Benchmark;
 
 /**
  * Created by milleruntime on 4/17/17.
  */
-public class PrintTests {
+public class TestUtils {
   // Add new classes to this array
   // All classes containing Benchmarks
   public static Class [] ALL_CLASSES = { MyBenchmark.class, UnoBenchmarkIT.class, InMemoryMapBenchmark.class, VersioningIterBenchmark.class };
 
   public static void main(String[] args) {
+    for(String a : args)
+      switch (a) {
+        case "-p":case "--print":
+          printTests();
+          break;
+        case "-v":case "--version":
+          printVersion();
+          break;
+        default:
+          System.out.println("Unknown option passed to TestUtils");
+      }
+
+  }
+
+  private static void printVersion() {
+    System.out.println("Running tests with Accumulo Iterator Stack Version: " + TestIteratorStack.VERSION);
+  }
+
+  private static void printTests() {
     List<String> methods = new ArrayList<>();
 
     for(Class clazz : ALL_CLASSES) {
@@ -30,6 +51,5 @@ public class PrintTests {
     for(String method : methods) {
       System.out.println(method);
     }
-
   }
 }
