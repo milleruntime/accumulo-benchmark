54d1abbc518b785714c0ffa07f23440524a1cf22
Mike Miller <mmiller@apache.org>
2018-01-31 16:07:09 -0500

    Change 1.8.2 to 1.9.0

--- ais-1.8.2/src/main/java/org/apache/TestIteratorStack.java
+++ ais-1.8.2/src/main/java/org/apache/TestIteratorStack.java
@@ -30,7 +30,7 @@ import org.apache.accumulo.core.security.Authorizations;
  * Class used for testing Accumulo Iterator Stack
  */
 public class TestIteratorStack {
-  public static final String VERSION = "1.8.2-SNAPSHOT";
+  public static final String VERSION = "1.9.0-SNAPSHOT";
   protected SortedKeyValueIterator<Key,Value> iterStack = null;
   protected AtomicLong scannedCount;
   protected AtomicLong seekCount;
